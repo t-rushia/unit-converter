aafa41c5bdb9ec4ed637ca13334a62c94ab4d705
Thomas Rushia <thomas.rushia@outlook.com>
2023-07-18 17:31:02 -0400

    Added three functions getLength getVolume and getMass which all take unit parameter and calculate the unit conversion

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 let unit = "";
 let inputText = document.querySelector("#input-text");
 const convertBtn = document.querySelector("#convert-btn");
-const length = document.querySelector("#length");
-const volume = document.querySelector("#volume");
-const mass = document.querySelector("#mass");
+const convertLength = document.querySelector("#length-el");
+const convertVolume = document.querySelector("#volume-el");
+const convertMass = document.querySelector("#mass-el");
 
 convertBtn.addEventListener("click", function () {
   if (inputText.value === "" || isNaN(inputText.value)) {
-    alert("Invalid entry, please enter a number.");
+    alert("Invalid entry, please enter a number");
     return;
-  } else {
-    unit = inputText.value;
   }
+  let unit = parseInt(inputText.value);
   getLength(unit);
   getVolume(unit);
   getMass(unit);
@@ -23,9 +22,19 @@ convertBtn.addEventListener("click", function () {
 */
 
 function getLength(unit) {
-  let result = unit * 3.281;
+  let totalFeet = unit * 3.281;
+  let totalMeters = unit * 0.3048;
+  convertLength.textContent = `${unit} meters = ${totalFeet} feet | ${unit} feet = ${totalMeters} meters`;
 }
 
-function getVolume(unit) {}
+function getVolume(unit) {
+  let totalGallons = unit * 0.264;
+  let totalLiters = unit * 3.78541;
+  convertVolume.textContent = `${unit} liters = ${totalGallons} gallons | ${unit} gallons = ${totalLiters} liters`;
+}
 
-function getMass(unit) {}
+function getMass(unit) {
+  let totalPounds = unit * 2.204;
+  let totalKilos = unit * 0.453592;
+  convertMass.textContent = `${unit} kilos = ${totalPounds} pounds | ${unit} pounds = ${totalKilos} kilos`;
+}
